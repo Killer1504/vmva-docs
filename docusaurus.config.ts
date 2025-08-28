95baabf22e78d1fac764b1cb449aaad6a7ffed27
hung le <thanhhungbkhn@gmail.com>
2025-08-28 11:37:31 +0700

    add v icon

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -7,7 +7,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 const config: Config = {
   title: 'VMA Documentation',
   tagline: 'An application for making video',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/v.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
